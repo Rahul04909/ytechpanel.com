8023c45121c7a70c712bfc96f223b118adf757a0
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-06-26 19:07:09 +0530

    updated

--- admin/footer.php
+++ admin/footer.php
@@ -7,7 +7,7 @@
 
         <footer class="main-footer">
             <div class="float-right d-none d-sm-inline">
-                Admin Panel Designed By <strong><a target="_blank" href="https://www.prayagcomputer.in">Rahul Dhiman / +91-8059982049</a></strong>
+                Admin Panel Designed By <strong><a target="_blank" href="https://www.mineib.com">Mineib Creative Technology</a></strong>
             </div>
             <strong> &copy;<?php echo date('Y'); ?></strong> All rights reserved.
         </footer>
